403618e3b8baf3a67047b426372086760e5a5b1f
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 12:07:20 +0800

    Add a component, Description, to Page1.

--- debug_1/my-app/src/components/Page1/Page1.js
+++ debug_1/my-app/src/components/Page1/Page1.js
@@ -1,12 +1,17 @@
 import React from "react";
 import "./Page1.css";
-import ImageObj from "../ImageObj/ImageObj";
+import Description from "../Description/Description";
 import VideoObj from "../VideoObj/VideoObj";
 
 const Page1 = () => {
+
+    const title = "test";
+    const content = "test";
+
     return (
         <div id="page1">
             <div className="page1__container">
+                <Description title={title} content={content}/>
                 <VideoObj/>
             </div>
         </div>
