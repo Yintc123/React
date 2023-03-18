6ea5560c844248b4c749228ebc5ea7fcf76ff314
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 20:53:42 +0800

    Update the page1.

--- my-app/src/pages/debug_1/Page1/Page1.js
+++ my-app/src/pages/debug_1/Page1/Page1.js
@@ -8,8 +8,8 @@ const Page1 = () => {
     const title = "Error：Uncaught (in promise) DOMException: The play() request was interrupted by a call to pause().";
     const content = "瀏覽器在 loading 影片資源的時候，被 pause 中斷。";
 
-    const muted = "muted"; // muted，實作 solution；null，實作 error
-    const flag = 0; // flag=0，實作 solution；flag=1，實作 error
+    const muted = "muted"; // muted，實作 debug；null，實作 error
+    const flag = 0; // flag=0，實作 debug；flag=1，實作 error
 
     return (
         <div id="page1">
