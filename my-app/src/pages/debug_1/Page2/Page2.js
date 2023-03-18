3cf698b1f470465f8cda5e0468795604df09e3b6
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 20:50:00 +0800

    Add a flag in the page2.

--- my-app/src/pages/debug_1/Page2/Page2.js
+++ my-app/src/pages/debug_1/Page2/Page2.js
@@ -3,10 +3,13 @@ import "./Page2.css";
 import VideoObj from "../../../components/VideoObj/VideoObj";
 
 const Page2 = () => {
+
+    const flag = 1; // flag=0，實作 solution；flag=1，實作 error
+
     return (
         <div id="page2">
             <div className="page2__container">
-                <VideoObj/>
+                <VideoObj flag={flag}/>
             </div>
         </div>
     )
