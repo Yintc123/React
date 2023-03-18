c025acd3be461987b4eb78fd70c3a41da0cb8920
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 11:46:28 +0800

    Update the component, showcase.

---
+++ debug_1/my-app/src/components/Showcase/Showcase.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Showcase.css";
+
+const Showcase = () => {
+    return (
+        <div id="showcase">
+            
+        </div>
+    )
+}
+
+export default Showcase;
