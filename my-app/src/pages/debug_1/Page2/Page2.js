be180f2c6a454437bb3f74672e05efff947add93
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 20:56:18 +0800

    Add a component, description, in the page2.

--- my-app/src/pages/debug_1/Page2/Page2.js
+++ my-app/src/pages/debug_1/Page2/Page2.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Page2.css";
+import Description from "../../../components/Description/Description";
 import VideoObj from "../../../components/VideoObj/VideoObj";
 
 const Page2 = () => {
+
+    const title = "Error：Uncaught (in promise) DOMException: The play() request was interrupted by a call to pause().";
+    const content = "瀏覽器在 loading 影片資源的時候，被 pause 中斷。";
 
     const flag = 1; // flag=0，實作 debug；flag=1，實作 error
     const muted = "muted"; // muted，實作 debug；null，實作 error
@@ -10,6 +14,7 @@ const Page2 = () => {
     return (
         <div id="page2">
             <div className="page2__container">
+                <Description title={title} content={content}/>
                 <VideoObj flag={flag}/>
             </div>
         </div>
