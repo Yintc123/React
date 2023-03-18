15a86fd560de4df3a0c0b1554f2183e786448f8a
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 20:50:53 +0800

    Add a flag in the page3.

--- my-app/src/pages/debug_1/Page3/Page3.js
+++ my-app/src/pages/debug_1/Page3/Page3.js
@@ -3,6 +3,9 @@ import "./Page3.css";
 import VideoObj from "../../../components/VideoObj/VideoObj";
 
 const Page3 = () => {
+
+    const flag = 0; // flag=0，實作 solution；flag=1，實作 error
+
     return (
         <div id="page3">
             <div className="page3__container">
