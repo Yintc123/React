33b6ebc8730a57cceb85aaaa7fcbff6589797839
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 19:29:54 +0800

    Update the page, Page1, and the component, VideoObj.

--- my-app/src/components/VideoObj/VideoObj.js
+++ my-app/src/components/VideoObj/VideoObj.js
@@ -1,15 +1,21 @@
 import React, { useEffect } from "react";
 import "./VideoObj.css";
 
-const VideoObj = () => {
+const VideoObj = (props) => {
     useEffect(() => {
         const video = document.querySelector("#videoObj__video");
-        console.log(video);
-        video.play();
-        video.pause();
+
+        if (props.flag==0) {
+            video.play().then(()=> {
+                video.pause();
+            })
+        }else {
+            video.play();
+            video.pause();
+        }
     }, [])
     return (
-        <video id="videoObj__video" controls muted="muted">
+        <video id="videoObj__video" controls muted={props.muted}>
             <source src="https://static.pexels.com/lib/videos/free-videos.mp4" type="video/mp4"></source>
         </video>
     )
