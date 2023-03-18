c999cf55347eeff94ea9412113d0444a2eeac484
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 20:55:50 +0800

    Remove the component, Description.

--- my-app/src/pages/debug_1/Page1/Page1.js
+++ my-app/src/pages/debug_1/Page1/Page1.js
@@ -1,12 +1,8 @@
 import React from "react";
 import "./Page1.css";
-import Description from "../../../components/Description/Description";
 import VideoObj from "../../../components/VideoObj/VideoObj";
 
 const Page1 = () => {
-
-    const title = "Error：Uncaught (in promise) DOMException: The play() request was interrupted by a call to pause().";
-    const content = "瀏覽器在 loading 影片資源的時候，被 pause 中斷。";
 
     const muted = "muted"; // muted，實作 debug；null，實作 error
     const flag = 0; // flag=0，實作 debug；flag=1，實作 error
@@ -14,7 +10,6 @@ const Page1 = () => {
     return (
         <div id="page1">
             <div className="page1__container">
-                <Description title={title} content={content}/>
                 <VideoObj muted={muted} flag={flag}/>
             </div>
         </div>
