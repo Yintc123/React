4d4237148d0e3ad54ea6bb0b47303f8fe99caae5
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 20:53:23 +0800

    Update the page2.

--- my-app/src/pages/debug_1/Page2/Page2.js
+++ my-app/src/pages/debug_1/Page2/Page2.js
@@ -4,7 +4,8 @@ import VideoObj from "../../../components/VideoObj/VideoObj";
 
 const Page2 = () => {
 
-    const flag = 1; // flag=0，實作 solution；flag=1，實作 error
+    const flag = 1; // flag=0，實作 debug；flag=1，實作 error
+    const muted = "muted"; // muted，實作 debug；null，實作 error
 
     return (
         <div id="page2">
