9f1a337b5c3cc1279e53643f8defd31d826e6e97
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-03-18 21:00:38 +0800

    Update the description in the page3.

--- my-app/src/pages/debug_1/Page3/Page3.js
+++ my-app/src/pages/debug_1/Page3/Page3.js
@@ -1,8 +1,12 @@
 import React from "react";
 import "./Page3.css";
+import Description from "../../../components/Description/Description";
 import VideoObj from "../../../components/VideoObj/VideoObj";
 
 const Page3 = () => {
+
+    const title = "Error：Uncaught (in promise) DOMException: play() failed because the user didn't interact with the document first.";
+    const content = "自動播放影片或音源未預設靜音。";
 
     const flag = 0; // flag=0，實作 debug；flag=1，實作 error
     const muted = null; // muted，實作 debug；null，實作 error
@@ -10,6 +14,7 @@ const Page3 = () => {
     return (
         <div id="page3">
             <div className="page3__container">
+                <Description title={title} content={content}/>
                 <VideoObj flag={flag} muted={muted}/>
             </div>
         </div>
