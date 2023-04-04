91c474c3f385f0cd0eb64459e24c9fc3ad63641e
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-04-04 15:07:30 +0800

    Use the component made by react-bootstrap.

--- my-app/src/components/NavBar/NavBar.js
+++ my-app/src/components/NavBar/NavBar.js
@@ -1,18 +1,48 @@
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
-import DropdownComp from "../DropdownComp/DropdownComp";
 import "./NavBar.css";
 
 const NavBar = () => {
     return (
         <div id="navBar">
-            <ul className="navBar__ul">
-                <li className="navBar__li"><a className="navBar__a" href="/React/">Home</a></li>
-                <li className="navBar__li"><a className="navBar__a" href="/React/page1">Page1</a></li>
-                <li className="navBar__li"><a className="navBar__a" href="/React/page2">Page2</a></li>
-                <li className="navBar__li"><a className="navBar__a" href="/React/page3">Page3</a></li>
-                <DropdownComp />
-            </ul>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="/React/">HOME</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <NavDropdown title="Learning" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Debugging" id="basic-nav-dropdown">
+                                <NavDropdown.Item href="/React/page1">Page1</NavDropdown.Item>
+                                <NavDropdown.Item href="/React/page2">Page2</NavDropdown.Item>
+                                <NavDropdown.Item href="/React/page3">Page3</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">
+                                    Another action
+                                </NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">
+                                    Separated link
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
         </div>
     )
 }
