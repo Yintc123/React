36ca952126cf6ccd66ef166d5421cff85596d296
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-04-04 21:50:14 +0800

    Add a note in the page, Page3.

--- my-app/src/pages/debug_1/Page3/Page3.js
+++ my-app/src/pages/debug_1/Page3/Page3.js
@@ -7,6 +7,7 @@ const Page3 = () => {
 
     const title = "Error：Uncaught (in promise) DOMException: play() failed because the user didn't interact with the document first.";
     const content = "自動播放影片或音源未預設靜音。";
+    const note = "註：開啟開發人員工具的主控台並且重新整理頁面即會顯示錯誤於主控台";
 
     const flag = 0; // flag=0，實作 debug；flag=1，實作 error
     const muted = false; // true，實作 debug；false，實作 error
@@ -14,7 +15,7 @@ const Page3 = () => {
     return (
         <div id="page3">
             <div className="page3__container">
-                <Description title={title} content={content}/>
+                <Description title={title} content={content} note={note}/>
                 <VideoObj flag={flag} muted={muted}/>
             </div>
         </div>
