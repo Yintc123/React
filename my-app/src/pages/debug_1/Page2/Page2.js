217bd55589094758857bd304cc8ec0bd2ce2289a
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-04-04 21:49:51 +0800

    Add a note in the page, Page2.

--- my-app/src/pages/debug_1/Page2/Page2.js
+++ my-app/src/pages/debug_1/Page2/Page2.js
@@ -7,6 +7,7 @@ const Page2 = () => {
 
     const title = "Error：Uncaught (in promise) DOMException: The play() request was interrupted by a call to pause().";
     const content = "瀏覽器在 loading 影片資源的時候，被 videoElem.pause() 中斷。";
+    const note = "註：開啟開發人員工具的主控台即會顯示錯誤於主控台";
 
     const flag = 1; // flag=0，實作 debug；flag=1，實作 error
     const muted = true; // true，實作 debug；false，實作 error
@@ -14,7 +15,7 @@ const Page2 = () => {
     return (
         <div id="page2">
             <div className="page2__container">
-                <Description title={title} content={content}/>
+                <Description title={title} content={content} note={note}/>
                 <VideoObj flag={flag} muted={muted}/>
             </div>
         </div>
