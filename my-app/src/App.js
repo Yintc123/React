843c625c63414fadc95accac0b9eb46ee4244a8a
Yintc123 <92730056+Yintc123@users.noreply.github.com>
2023-04-04 15:06:57 +0800

    Add CSS of bootstrap.

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // import logo from './logo.svg';
 import './App.css';
 import NavBar from "./components/NavBar/NavBar";
